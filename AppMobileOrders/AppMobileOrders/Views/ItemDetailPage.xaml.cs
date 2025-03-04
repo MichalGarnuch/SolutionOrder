5da7f9ec7b8d684d242206e5e1674817818a96bd
Michal <m.p.garnuch@gmail.com>
2025-03-04 18:53:02 +0100

    Add project files.

---
+++ AppMobileOrders/AppMobileOrders/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using Xamarin.Forms;
+using AppMobileOrders.ViewModels;
+
+namespace AppMobileOrders.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
